e0a8dabe165204c51f08b7ef9800bb9827359a2e
laozhao <3@3.com>
2017-01-19 00:34:07 +0800

    提交

--- zhongyiNodeJs/routes/admin2.js
+++ zhongyiNodeJs/routes/admin2.js
@@ -2,14 +2,37 @@ var express = require('express');
 var url = require('url');
 var router = express.Router();
 var baseRouter = require('../routes/base');
-
+//默认首页
 router.get('/',function(req,res){
     res.render('managerv2/dashboard',baseRouter.setPageCurrentInfo(req,res,'xx'));
 });
- 
-
+ //用户列表
 router.get('/userlist',function(req,res){
-    console.log('xx');
     res.render('managerv2/dock2',adminFunc.setPageInfov2(req,res,'xx'));
+});
+//登录
+router.get('/login',function(req,res){
+    res.render('managerv2/pageslogin');
+});
+//登录验证
+router.post('/login',function(req,res){
+    var _uname=req.body.username;
+    var _pwd=req.body.password;
+    if(_uname!="admin"){
+        console.log(_uname);
+          res.send('账号不对'+_uname);
+    }
+   
+
+     if(_pwd!="admin")
+     {
+          console.log(_pwd);
+          res.send('密码不对'+_pwd);
+     }
+    
+     
+
+    res.redirect('/adminV2');
+   
 });
 module.exports = router;
