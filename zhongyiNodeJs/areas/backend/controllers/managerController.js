d96324bb29e9905e2fe179cbdc5af9937b0d4f85
xiaopohou <393331943@qq.com>
2017-01-24 13:30:51 +0800

    update

--- zhongyiNodeJs/areas/backend/controllers/managerController.js
+++ zhongyiNodeJs/areas/backend/controllers/managerController.js
@@ -3,15 +3,19 @@ var baseController = require('../../../routes/base');
 
 module.exports = {
     get_index: function (req, res) {
-        res.render('manager/index2',baseController.setPageCurrentInfo(req,res));
+        //res.render('manager/index2',baseController.setPageCurrentInfo(req,res));
+        res.render(
+            'manager/index2',
+            {
+                title: "hello",
+                "layout": "manager/_layoutView.ejs"
+                ,"foo": "hh"
+                ,"pageSectionA":"xxx"
+            }
+        );
     },
     get_login: function (req, res) {
         res.render('manager/login');
-    },
-    get_loginout: function (req, res) {
-        res.render('manager/loginout');
-    },
-        get_test: function (req, res) {
-        res.render('manager/test/index');
     }
+
 };
--- zhongyiNodeJs/routes/base.js
+++ zhongyiNodeJs/routes/base.js
@@ -1,14 +1,14 @@
 /**
  * Created by duzhong on 16/7/22.
  */
-var siteConfig = require('../public/config/zy_Config');
+//var siteConfig = require('../public/config/zy_Config');
 
 var adminFunc = {
 
     setPageCurrentInfo: function (req, res) {
         return {
             //siteInfo: this.siteInfos(module),
-            layout: '/managerv2/layout'
+            layout: 'manager/layout'
         }
     }
 };
