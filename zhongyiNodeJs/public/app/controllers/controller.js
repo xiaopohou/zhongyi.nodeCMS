e906679b2c30f13b0a21574232177fc7eae9682a
laozhao <3@3.com>
2017-02-04 00:47:29 +0800

    update

--- zhongyiNodeJs/public/app/controllers/controller.js
+++ zhongyiNodeJs/public/app/controllers/controller.js
@@ -1,3 +1,5 @@
+var nodeApp = angular.module('nodeApp',[]);
+
 nodeApp.controller('roleController',['$scope','$http',function($scope,$http){
     $scope.fuck=function(){
         alert('fuck you everyday !');
