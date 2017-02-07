a6f8cf9e434541098d055571e9c79141b88a1652
xiaopohou <393331943@qq.com>
2017-02-07 18:02:13 +0800

    .

--- zhongyiNodeJs/public/app/controllers/controller.js
+++ zhongyiNodeJs/public/app/controllers/controller.js
@@ -1,9 +1,18 @@
-var nodeApp = angular.module('adminApp',[]);
+var nodeApp = angular.module('adminApp', []);
 
-nodeApp.controller('roleController',['$scope','$http',function($scope,$http){
-    $scope.fuck=function(){
+nodeApp.controller('roleController', ['$scope', '$http', function ($scope, $http) {
+    $scope.fuck = function () {
         alert('fuck you everyday !');
-         
     };
-   
+    //弹出信息添加
+    $scope.showinfo = function () {
+        $.modalOpen({
+            id: "Form",
+            title: "新增角色",
+            url: "/backend/role/index2",
+            width: "550px",
+            height: "570px",
+            btn: null
+        });
+    }
 }]);
