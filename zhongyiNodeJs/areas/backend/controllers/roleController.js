236ad62a03b37b582e505fb365917572c31abd02
xiaopohou <393331943@qq.com>
2017-02-14 15:40:52 +0800

    .

--- zhongyiNodeJs/areas/backend/controllers/roleController.js
+++ zhongyiNodeJs/areas/backend/controllers/roleController.js
@@ -1,7 +1,7 @@
 var basePage = require('../../../core/baseController');
 var roleModel=require('../../../models/zyModels/Role');
 var dbOperator=require('../../../models/zyDBHelper/zy_Dbopt');
- 
+var ResponseData= require('../../../utils/responseData');
 module.exports={
     get_index:function(req,res){
        //res.send('index');
@@ -12,25 +12,31 @@ module.exports={
     },
     get_role:function(req,res,id)
     {
+        var _responseData=new ResponseData();
+ 
         roleModel.findOne({"_id":id},function(err,doc){ 
              if(err)
              {
-                res.end(err);
+               _responseData.errorMessage="出错了";
+                res.json(_responseData);
              }else
              {
-                res.json(doc);
+                 _responseData.data=doc;
+                res.json(_responseData);
              }
         }); 
+        
     },
     post_addrole:function(req,res)
     {
          //获取参数
         var name = req.body.name;
+ 
         var role=new roleModel({
             name:name
         });
         role.save(function(err){
-            
+                    
         });
         res.send('success');
     },
--- zhongyiNodeJs/public/app/controllers/controller.js
+++ zhongyiNodeJs/public/app/controllers/controller.js
@@ -1,36 +1,36 @@
-var nodeApp = angular.module('adminApp',[]);
+var nodeApp = angular.module('adminApp', []);
 
 nodeApp.controller('roleController', ['$scope', '$http', function ($scope, $http) {
-    $scope.formData={};
-    $scope.refresh = function () {
-        alert('fuck you everyday !');
-    };
-    
-    //分页 
-    initPagination($scope,$http,'/common/getDocumentList/role','normalList');
-
+    $scope.formData = {};
      
+    //分页 
+    initPagination($scope, $http, '/common/getDocumentList/role', 'normalList');
 
     $scope.addrole = function (id) {
-        if(!id)
-        {
-            $.modalOpen({
-            id: "Form",
-            title: "编辑角色",
-            url: "/backend/role/addrole",
-            width: "550px",
-            height: "370px",
-            btn: null
+
+
+    $scope.formData.name="xxxxxxxxxxxxxxxxxxxxxxx";
+
+        //编辑
+        if (id != "") {
+            var url = '/backend/role/role/' + id;
+            $http.get(url).success(function (res) {
+                //alert(res.data.name);
+ 
+                $.modalOpen({
+                    id: "Form",
+                    title: "编辑角色",
+                    url: "/backend/role/addrole",
+                    width: "550px",
+                    height: "370px",
+                    btn: null
+                });
+
             });
         }
-        else
-        {
-             
-            ngGet($http,'/backend/role/role/'+id,'',function(data){
-                $scope.formData=data;
-            });
+        else {
 
-                $.modalOpen({
+            $.modalOpen({
                 id: "Form",
                 title: "新增角色",
                 url: "/backend/role/addrole",
@@ -41,25 +41,25 @@ nodeApp.controller('roleController', ['$scope', '$http', function ($scope, $http
         }
 
     };
-    $scope.processForm=function(isValid)
-    {
-      var roleData=
-      {
-          name:$scope.formData.name
-      };
-        if(isValid)
-        {
-            ngPost($http,isValid,"/backend/role/addrole",$scope.formData,function(data){
+
+    $scope.processForm = function (isValid) {
+        var roleData =
+            {
+                name: $scope.formData.name
+            };
+
+            
+        if (isValid) {
+            ngPost($http, isValid, "/backend/role/addrole", $scope.formData, function (data) {
                 //表格刷新
-               top.window.location.reload();
+                top.window.location.reload();
             });
         }
 
     },
-    $scope.test1=function()
-    {
-        window.location="/backend/role/test1/laozhao";
-    }
+        $scope.test1 = function () {
+            window.location = "/backend/role/test1/laozhao";
+        }
 }]);
 
 
