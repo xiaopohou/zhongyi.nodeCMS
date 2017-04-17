dd70455e3ab239c34e9382abf121ffe2d85ad1e8
xiaopohou <393331943@qq.com>
2017-04-17 15:33:05 +0800

    .

--- zhongyiNodeJs/app.js
+++ zhongyiNodeJs/app.js
@@ -104,7 +104,7 @@ resolve
 app.use(partials());
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 
 //日志 
 // var logDir = __dirname + '/logs/access';
