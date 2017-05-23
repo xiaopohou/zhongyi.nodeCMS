376fcfb3cc67f8a6392273d8f279f2c421cdd65d
laozhao <3@3.com>
2017-05-23 23:53:59 +0800

    .

--- zhongyiNodeJs/app.js
+++ zhongyiNodeJs/app.js
@@ -110,7 +110,7 @@ app.use(partials());
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 
 
 //log
